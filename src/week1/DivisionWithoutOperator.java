452d81d18528773bc3135c2f7b8119e6068d83f8
Irinlupu <irinlupu@gmail.com>
2023-05-21 18:02:07 -0400

    Created: => DivisionWithoutOperator.java => FinRa.java => OddOrEven.java => InterviewCodingTasks.txt => TBD.txt

---
+++ src/week1/DivisionWithoutOperator.java
@@ -0,0 +1,16 @@
+package week1;
+
+public class DivisionWithoutOperator {
+    /*
+    Numbers -- Divide without / operator
+            => Write a method that can divide two numbers without using division operator.
+     */
+
+    // Solution Below
+
+
+
+
+
+
+}
---
+++ src/week1/FinRa.java
@@ -0,0 +1,14 @@
+package week1;
+
+public class FinRa {
+
+    /*
+    Numbers -- FINRA
+        => Write a method that prints out the numbers from 1 to 30 but;
+            - for numbers which are multiple of 3 print "FIN" instead of the number,
+            - for numbers which are multiple of 5, print "RA" instead of the number,
+            - and for numbers which are multiple of both 3 and 5, print "FINRA" instead of the number.
+     */
+
+    // Solution below
+}
