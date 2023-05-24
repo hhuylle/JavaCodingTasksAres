333c1f692d7566b05bc3545b1d77743a6872c608
laila1996 <lmajidi@students.pccc.edu>
2023-05-24 14:43:23 -0400

    DivisionWithoutOperator is Done

--- src/week1/DivisionWithoutOperator.java
+++ src/week1/DivisionWithoutOperator.java
@@ -1,16 +1,28 @@
 package week1;
 
+import javax.print.attribute.standard.PresentationDirection;
+
 public class DivisionWithoutOperator {
     /*
     Numbers -- Divide without / operator
             => Write a method that can divide two numbers without using division operator.
      */
 
-    // Solution Below
+     public static void divideWithoutOperator(double a,double b){
+
+             int count=0;
+             while (a>=b) {
+                  a-=b;
+                 count++;
+             }
+             System.out.println(count+" reminder "+a);
+         }
 
 
+    public static void main(String[] args) {
+        divideWithoutOperator(14,3);
 
-
+    }
 
 
 }
