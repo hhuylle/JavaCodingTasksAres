0e537cfdf69d2d958dec21bfa76dc8c1d2a98e97
Cydeo-Huy <hhuylle@gmail.com>
2023-05-25 22:04:04 -0400

    Created evenOrOdd method in java

---
+++ src/HuyL_Week1/EvenAndOdd.java
@@ -0,0 +1,15 @@
+package HuyL_Week1;
+
+public class EvenAndOdd {
+
+    public static void evenOrOdd(int number){
+
+        if (number%2 == 0){
+            System.out.println("Even");
+        }else {
+            System.out.println("Odd");
+        }
+    }
+
+
+}
