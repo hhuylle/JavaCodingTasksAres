79b362f036cfcea1a4bd4c850948b2ff67daf717
Cydeo-Huy <hhuylle@gmail.com>
2023-05-31 10:26:48 -0400

    Created SwapNumbers program in java

---
+++ src/HuyL_Week2/SwapNumbers.java
@@ -0,0 +1,16 @@
+package HuyL_Week2;
+
+public class SwapNumbers {
+
+    public static void main(String[] args) {
+
+        int num1 = 20, num2 = 15;
+
+        System.out.println((num2=(num1+num2)-num1)+" | "+ (num1=(num1+num2)-num2));
+
+    }
+}
+/*
+Swap Numbers
+Swap two variables' values without using a third variable.
+ */
