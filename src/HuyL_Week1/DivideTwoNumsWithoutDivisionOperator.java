8276ed7b3a5e92c534f22215ee0703d4dd580cc8
Cydeo-Huy <hhuylle@gmail.com>
2023-05-31 08:52:58 -0400

    Created DivideTwoNumsWithoutDivisionOperator method in java

---
+++ src/HuyL_Week1/DivideTwoNumsWithoutDivisionOperator.java
@@ -0,0 +1,27 @@
+package HuyL_Week1;
+
+public class DivideTwoNumsWithoutDivisionOperator {
+
+    public static void DivisionWithoutDivisionOperator(int num1, int num2){
+
+        if (num1 > 0 && num2 > 0 ){
+
+            int division = 0;
+
+            while (num1 >= num2){
+                if (num1>0 && num2>0) {
+                    num1 = num1 - num2;
+                    division = division + 1;
+                }else {
+                    System.out.println("Invalid number");
+                    System.exit(1);
+                }
+            }
+            System.out.println("Division = "+division);
+        }else {
+            System.out.println("Invalid numbers");
+            System.exit(1);
+        }
+
+    }
+}
