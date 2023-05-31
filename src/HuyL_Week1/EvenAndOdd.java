19adad5df509a2325327d4b8949c2a4f3b0bead2
Cydeo-Huy <hhuylle@gmail.com>
2023-05-31 08:50:12 -0400

    Updated evenOrOdd method in java

--- src/HuyL_Week1/EvenAndOdd.java
+++ src/HuyL_Week1/EvenAndOdd.java
@@ -4,10 +4,15 @@ public class EvenAndOdd {
 
     public static void evenOrOdd(int number){
 
-        if (number%2 == 0){
-            System.out.println("Even");
+        if (number<=0){
+            System.out.println("Number can not be zero or negative ");
+            System.exit(1);
         }else {
-            System.out.println("Odd");
+            if (number%2 == 0){
+                System.out.println("Even");
+            }else {
+                System.out.println("Odd");
+            }
         }
     }
 
